cf6f977ab80b8a815f239e73bfa72505decb3c7d
singstep <zhang.yaorong@qq.com>
2017-12-09 17:14:35 +0800

    process encoded data

--- dataProcess/dataRestore.py
+++ dataProcess/dataRestore.py
@@ -1,36 +1,49 @@
+import os
 
-insuranceVocb={}
-vocbFilePath="../insuranceData/vocabulary"
-vocbFile=open(vocbFilePath)
-line=vocbFile.readline()
-while line!=None and line!="":
-    record=line.split("\t")
-    insuranceVocb[record[0]]=record[1].rstrip("\n")
+def restore_file(source_filepath,target_filepath):
+    source_file=open(source_filepath,'r')
+    target_file=open(target_filepath,'w')
+    line=source_file.readline()
+    while line is not None and line!="":
+        record = line.split("\t")
+        index = record[0]
+        words = record[1].split(" ")
+        for i in range(len(words)):
+            key = words[i]
+            word = insuranceVocb.get(key)
+            if word is not None:
+                words[i] = word
+        token = " "
+        newline = index + "\t" + token.join(words)
+        if len(record)>2:
+            for i in range (2,len(record)):
+                newline+="\t"+record[i]
+        target_file.write(newline)
+        line = source_file.readline()
+    source_file.close()
+    target_file.close()
+    print("restored:",source_file.name," to:",target_file.name)
+    return
+
+if __name__=='__main__':
+    insuranceVocb={}
+    vocbFilePath="../insuranceData/raw/vocabulary"
+    vocbFile=open(vocbFilePath)
     line=vocbFile.readline()
+    while line!=None and line!="":
+        record=line.split("\t")
+        insuranceVocb[record[0]]=record[1].rstrip("\n")
+        line=vocbFile.readline()
 
-print("size-->",len(insuranceVocb))
-vocbFile.close()
+    print("vocb size-->",len(insuranceVocb))
+    vocbFile.close()
 
-answerEncodeFilePath="../insuranceData/InsuranceQA.label2answer.raw.encoded"
-answerEncodeFile=open(answerEncodeFilePath,'r')
-answerFilePath="../insuranceData/InsuranceQA.label2answer.raw"
-answerFile=open(answerFilePath,'w')
+    encoded_file_dir="../insuranceData/encoded"
+    raw_file_dir="../insuranceData/raw"
+    qafiles = os.listdir(encoded_file_dir)
+    for filename in qafiles:
+        source_file_path=encoded_file_dir+"/"+filename
+        if "test" in filename or "train" in filename or "valid" in filename:
+            target_file_path = raw_file_dir+"/"+filename[0:filename.rfind(".encoded")]
+            restore_file(source_filepath=source_file_path ,target_filepath=target_file_path)
 
-line=answerEncodeFile.readline()
-while line!=None and line!="":
-    record=line.split("\t")
-    index=record[0]
-    answerEncoded=record[1].split(" ")
-
-    for i in range(len(answerEncoded)):
-        key=answerEncoded[i]
-        word=insuranceVocb.get(key)
-        if word is not None:
-           answerEncoded[i]=word
-    token=" "
-    newline = index + "\t"+token.join(answerEncoded)
-    answerFile.write(newline)
-    line=answerEncodeFile.readline()
-answerEncodeFile.close()
-answerFile.close()
-print("ok")
--- dataProcess/test.py
+++ dataProcess/test.py
@@ -1,1 +1,12 @@
+import os
+
 print('你好')
+
+qafiles=os.listdir("../insuranceData")
+for filename in qafiles:
+    print(filename)
+    if "test" in filename or "train" in filename or "valid" in filename:
+        index=filename.rfind(".encoded")
+        if index>-1:
+            newfilename=filename[0:index]
+            print(newfilename)
